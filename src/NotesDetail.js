61df622f718d6a7b29a55e36dcaa757794d3c84f
Anthony DiRusso <ossurid@gmail.com>
2019-05-06 11:43:05 -0400

    changes note detail into a class using destructuring

--- src/NotesDetail.js
+++ src/NotesDetail.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function NotesDetail({ note }) {
-  return (
-    <div>
-      {note.text}
-    </div>
-  );
+export default class NotesDetail extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          
+      }
+  }
+  render() {
+      //declares the class name and note variables
+      //and assigns them to the property this.props
+      //where the name matches.
+      const { note } = this.props;
+      return (
+        <div>
+          {note.text}
+        </div>
+      );
+
+  }
 }
--- src/NotesList.js
+++ src/NotesList.js
@@ -14,6 +14,7 @@ function NotesListItem({ id, text, handleClick }) {
             >
             {text}
             </a></li>
+        
     );
 }
 
